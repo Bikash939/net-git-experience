de91eae984d16928fc82f7a33a8a5a6ec7b2a903
Bikash <bsmeher939@gmail.com>
2026-02-21 09:15:48 +0530

    modifed hello world to hello first modifed code

--- net-git-experience/Program.cs
+++ net-git-experience/Program.cs
@@ -10,7 +10,7 @@ namespace net_git_experience
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello first modified code!");
         }
     }
 }
